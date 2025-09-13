da22479c4b924808831bd97201074ad7d09adda8
Nesc <raymond059@mails.gdut.edu.cn>
2025-09-13 21:20:56 +0800

    vault backup: 2025-09-13 21:20:56

--- 200 刷题/[kuangbin带你飞]专题一 简单搜索/B - Dungeon Master.cpp
+++ 200 刷题/[kuangbin带你飞]专题一 简单搜索/B - Dungeon Master.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 queue<int> search_que;
 char map[32][32][32];
+
+typedef pair<int, int> RC;
+typedef pair<int, RC> LRC;
+
+int dx[6] ={-1, 0, 1, 0, 0, 0}; 
+int dy[6] ={0, -1, 0, 1, 0 ,0};
+int dz[6] ={0, 0, 0, 0, 1, -1};
+LRC end_point;
+LRC start_point;
 int L,R,C;
 //宽度优先搜索
 //需要用到队列
@@ -16,9 +25,18 @@ int main()
             for(int j = 0; j < C; j++)
             {
                 cin>>map[k][i][j];
+                if(map[k][i][j] == 'S')
+                {
+                    start_point = LRC(k,RC(i,j));
+                }
+                if(map[k][i][j] == 'E')
+                {
+                    end_point = LRC(k,RC(i,j));
+                }
             }
         }
     }
+    /*
     for(int k = 0; k < L; k++)
     {
         for(int i = 0; i < R; i++)
@@ -31,4 +49,5 @@ int main()
         }
         cout<<endl;
     }
+    */
 }
